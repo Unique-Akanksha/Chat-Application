fe1c1167c7e3c1e210f4881668fac2d9b644ae51
Akanksha Gupta <akankshagupta8322@gmail.com>
2026-06-04 12:21:08 +0530

    Module Setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AuthModule } from './module/auth/auth.module';
+import { UsersModule } from './module/users/users.module';
+import { ConversationsModule } from './module/conversations/conversations.module';
+import { MessagesModule } from './module/messages/messages.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConversationsModule,
+    MessagesModule,
+    MongooseModule.forRoot('mongodb://localhost/chat-application'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
